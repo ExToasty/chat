6f0a845bc6a92476cf1c93ef6adcd9912cce2cba
hadifaraz <hadifaraz52@protonmail.com>
2022-04-24 10:55:30 -0600

    Removed uses of InputMode enum.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,9 +1,7 @@
-pub mod input;
 pub mod ui;
 
 pub struct App {
     pub input: String,
-    pub input_mode: input::InputMode,
     pub messages: Vec<String>,
 }
 
@@ -11,7 +9,6 @@ impl Default for App {
     fn default() -> Self {
         App {
             input: String::from("Input text here: "),
-            input_mode: input::InputMode::Editing,
             messages: Vec::new(),
         }
     }
